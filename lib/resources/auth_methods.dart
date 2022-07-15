85588a17b84932b85f437d96e3929a1ddbf63d65
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2022-07-15 18:33:44 +0200

    Add image to storage on firebase

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone_flutter/resources/storage_method.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -21,6 +22,8 @@ class AuthMethods {
         //register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
         print(cred.user!.uid);
+
+        String photoUrl = await StorageMethods().uploadImageToStorage('profilePics', file, false);
         //add user to database
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
@@ -29,6 +32,7 @@ class AuthMethods {
           'bio': bio,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl,
         });
 
         res = "success";
